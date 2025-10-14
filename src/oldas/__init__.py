0303825c87edb846c1b2ea756338a2254dd9465c
Dave Pearson <davep@davep.org>
2025-10-14 09:13:41 +0100

    :card_index: Add the usual dunders to the library's init file

--- src/oldas/__init__.py
+++ src/oldas/__init__.py
@@ -1,2 +1,17 @@
-def main() -> None:
-    print("Hello from oldas!")
+"""TheOldReader API async client library."""
+
+##############################################################################
+# Python imports.
+from importlib.metadata import version
+
+######################################################################
+# Main library information.
+__author__ = "Dave Pearson"
+__copyright__ = "Copyright 2025, Dave Pearson"
+__credits__ = ["Dave Pearson"]
+__maintainer__ = "Dave Pearson"
+__email__ = "davep@davep.org"
+__version__ = version("oldas")
+__licence__ = "MIT"
+
+### __init__.py ends here
