75e4778891591f3014d1b9a2b63e732c4b4eab06
Dave Pearson <davep@davep.org>
2025-10-14 16:29:40 +0100

    :sparkles: Add support for loading subscriptions

--- src/oldas/__main__.py
+++ src/oldas/__main__.py
@@ -2,14 +2,15 @@ from asyncio import run
 from os import getenv
 
 from .session import Session
-from .user import User
+from .subscriptions import Subscriptions
 
 async def main() -> None:
     if token := getenv("TOR_TOKEN"):
         session = Session("test", token)
     else:
         session = await Session("test").login(getenv("TOR_USER", ""), getenv("TOR_PASSWORD", ""))
-    print(await User.load(session))
+    for subscription in await Subscriptions.load(session):
+        print(subscription.title)
 
 if __name__ == "__main__":
     run(main())
---
+++ src/oldas/subscriptions.py
@@ -0,0 +1,94 @@
+"""Provides a class for getting subscription information."""
+
+##############################################################################
+# Backward compatibility.
+from __future__ import annotations
+from typing import Any, NamedTuple
+
+##############################################################################
+# Local imports.
+from .session import Session
+
+##############################################################################
+class Category(NamedTuple):
+    """Holds details of a category."""
+
+    id: str
+    """The ID for the category."""
+    label: str
+    """The label for the category."""
+
+    @classmethod
+    def from_json(cls, data: dict[str, Any]) -> Category:
+        """Load the category from JSON data.
+
+        Args:
+            data: The data to load the category from.
+
+        Returns:
+            The category.
+        """
+        return cls(
+            id = data["id"],
+            label = data["label"],
+        )
+
+##############################################################################
+class Subscription(NamedTuple):
+    """Holds a subscription."""
+
+    id: str
+    """The ID of the subscription."""
+    title: str
+    """The title of the subscription."""
+    sort_id: str
+    """The sort ID of the subscription."""
+    first_item_time: int
+    """The time of the first item."""
+    url: str
+    """The URL of the subscription."""
+    html_url: str
+    """The HTML URL of the subscription."""
+    categories: list[Category]
+    """The Categories for the subscription."""
+
+    @classmethod
+    def from_json(cls, data: dict[str, Any]) -> Subscription:
+        """Load the subscription from JSON data.
+
+        Args:
+            data: The data to load the subscription from.
+
+        Returns:
+            The subscription.
+        """
+        return cls(
+            id = data["id"],
+            title = data["title"],
+            sort_id = data["sortid"],
+            first_item_time = data["firstitemmsec"],
+            url = data["url"],
+            html_url = data["htmlUrl"],
+            categories = [Category.from_json(category) for category in data["categories"]],
+        )
+
+##############################################################################
+class Subscriptions:
+    """Loads and holds the full list of subscriptions."""
+
+    @classmethod
+    async def load(cls, session: Session) -> list[Subscription]:
+        """Load the subscriptions.
+
+        Args:
+            session: The API session object.
+
+        Returns:
+            A list of subscriptions.
+        """
+        return [
+            Subscription.from_json(subscription) for subscription in
+            (await session.get("subscription/list"))["subscriptions"]
+        ]
+
+### subscriptions.py ends here
