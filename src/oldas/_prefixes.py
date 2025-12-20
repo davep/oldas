14443a2e3b99682494ae49849895ccac2166d81c
Dave Pearson <davep@davep.org>
2025-12-20 18:59:08 +0000

    :sparkles: Add utility functions for testing specific ID types

--- src/oldas/_prefixes.py
+++ src/oldas/_prefixes.py
@@ -26,4 +26,30 @@ def id_is_a(item_id: str, prefix: Prefix) -> bool:
     return item_id.startswith(prefix)
 
 
+##############################################################################
+def id_is_a_folder(item_id: str) -> bool:
+    """Does the ID look like it's a folder?
+
+    Args:
+        item_id: The ID to check.
+
+    Returns:
+        `True` if the ID looks like a folder, `False` if not.
+    """
+    return id_is_a(item_id, Prefix.FOLDER)
+
+
+##############################################################################
+def id_is_a_feed(item_id: str) -> bool:
+    """Does the ID look like it's a feed?
+
+    Args:
+        item_id: The ID to check.
+
+    Returns:
+        `True` if the ID looks like a feed, `False` if not.
+    """
+    return id_is_a(item_id, Prefix.FEED)
+
+
 ### _prefixes.py ends here
