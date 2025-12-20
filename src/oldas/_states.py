ca3ca87ccb603037e79915f47244903ff3b0b3a0
Dave Pearson <davep@davep.org>
2025-12-20 10:44:45 +0000

    :sparkles: Add more states

--- src/oldas/_states.py
+++ src/oldas/_states.py
@@ -15,6 +15,14 @@ class State(StrEnum):
     """An article that has been starred."""
     FRESH = "user/-/state/com.google/fresh"
     """An article that is considered fresh."""
+    KEPT_UNREAD = "user/-/state/com.google/kept-unread"
+    """An article was marked by the user to be kept unread."""
+    READING_LIST = "user/-/state/com.google/reading-list"
+    """The article is part of the user's reading list."""
+    BROADCAST = "user/-/state/com.google/broadcast"
+    """The article has been shared."""
+    LIKE = "user/-/state/com.google/like"
+    """The article has been liked."""
 
 
 ### _states.py ends here
